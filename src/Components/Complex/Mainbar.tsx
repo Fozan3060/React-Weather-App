556e6584641dbd405ff719d2fe60298d5ee0af45
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2024-08-03 20:19:19 +0500

    feat: added default images to sunevent

--- src/Components/Complex/Mainbar.tsx
+++ src/Components/Complex/Mainbar.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { TiWeatherDownpour } from "react-icons/ti";
 import { useWeatherContext } from "../../Context/WeatherContext";
 import LazySuneventCard from "../lazyloaded/LazySuneventCard";
 import LazyWeatherMetrics from "../lazyloaded/LazyWeatherMetrics";
@@ -39,9 +40,17 @@ const Mainbar: React.FC = () => {
               <WeatherForecastCard />
             </>
           ) : (
-            <h1 className="text-center mt-5 dark:text-white text-black">
-              No City Searched
-            </h1>
+            <div className="flex justify-center gap-5 md:gap-10   rounded-lg mt-10">
+              {Array.from({ length: 3 }, (_, index) => (
+                <div
+                  className="h-56 bg-opacity-30 bg-white w-28 rounded-[4rem]"
+                  key={index}
+                >
+                  <h1 className="mt-14 text-center">No city searched</h1>
+                  <TiWeatherDownpour className="m-auto mt-5" size={60} />
+                </div>
+              ))}
+            </div>
           )}
         </Suspense>
       </ErrorBoundary>
