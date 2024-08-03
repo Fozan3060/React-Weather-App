3792b2b02f141c65c44cdc2cab4846c68820a966
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2024-08-03 20:30:21 +0500

    feat: add default images to weahtermetrics

--- src/Components/Complex/Mainbar.tsx
+++ src/Components/Complex/Mainbar.tsx
@@ -40,17 +40,30 @@ const Mainbar: React.FC = () => {
               <WeatherForecastCard />
             </>
           ) : (
-            <div className="flex justify-center gap-5 md:gap-10   rounded-lg mt-10">
-              {Array.from({ length: 3 }, (_, index) => (
-                <div
-                  className="h-56 bg-opacity-30 bg-white w-28 rounded-[4rem]"
-                  key={index}
-                >
-                  <h1 className="mt-14 text-center">No city searched</h1>
-                  <TiWeatherDownpour className="m-auto mt-5" size={60} />
-                </div>
-              ))}
-            </div>
+            <>
+              <div className="flex justify-center gap-5 md:gap-10   rounded-lg mt-10">
+                {Array.from({ length: 3 }, (_, index) => (
+                  <div
+                    className="h-56 bg-opacity-30 bg-white w-28 rounded-[4rem]"
+                    key={index}
+                  >
+                    <h1 className="mt-14 text-center">No city searched</h1>
+                    <TiWeatherDownpour className="m-auto mt-5" size={60} />
+                  </div>
+                ))}
+              </div>
+              <div className="w-3/4 rounded-lg justify-center gap-sm-20 gap-6 flex h-10  m-auto bg-white bg-opacity-30">
+                {Array.from({ length: 3 }, (_, index) => (
+                  <div
+                    className=" bg-opacity-30 flex justify-center items-center gap-3 "
+                    key={index}
+                  >
+                    <h1 className="">NA</h1>
+                    <TiWeatherDownpour className=" " size={30} />
+                  </div>
+                ))}
+              </div>
+            </>
           )}
         </Suspense>
       </ErrorBoundary>
